d20771ca0bbde88b9b2ce1a335fdfc59da7cff69
ZackyPratama <rnxskyz@gmail.com>
2026-04-13 16:37:42 +0700

    Add fitur Category & Brand

---
+++ app/Filament/Resources/Brands/Schemas/BrandForm.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Filament\Resources\Brands\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Components\Section;
+use Filament\Schemas\Schema;
+
+class BrandForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Section::make('Informasi Brand')
+                    ->columns(2)
+                    ->columnSpanFull()
+                    ->schema([
+                        TextInput::make('name')
+                            ->required(),
+                        TextInput::make('slug')
+                            ->visibleOn('edit')
+                            ->readOnly()
+                            ->unique(ignoreRecord: true)
+                            ->required(),
+                        Textarea::make('description')
+                        ->rows(3)
+                        ->default(null)
+                            ->columnSpanFull(),
+                        TextInput::make('logo')
+                            ->image()
+                            ->directory('brands')
+                            ->disk('public')
+                            ->maxSize(2048) // 2MB
+                            ->imageEditor()
+                            ->default(null),
+                        TextInput::make('website')
+                            ->url()
+                            ->placeholder('https://example.com'),
+                    ]),
+
+                Section::make('Detail Tampilan')
+                    ->schema([
+                        Toggle::make('is_active')
+                            ->required(),
+                        TextInput::make('sort_order')
+                            ->required()
+                            ->numeric()
+                            ->default(0),
+                    ]),
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Categories/Schemas/CategoryForm.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Filament\Resources\Categories\Schemas;
+
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Components\Section;
+use Filament\Schemas\Schema;
+
+class CategoryForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Section::make('Informasi Kategori')
+                    ->columnSpanFull()
+                    ->columns(2)
+                    ->schema([
+                        TextInput::make('name')
+                            ->required(),
+                        TextInput::make('slug')
+                            ->unique(ignoreRecord: true)
+                            ->readOnly()
+                            ->visibleOn('edit'),
+                        Textarea::make('description')
+                            ->rows(3)
+                            ->default(null)
+                            ->columnSpanFull(),
+                        FileUpload::make('image')
+                            ->disk('public')
+                            ->directory('categories')
+                            ->imageEditor()
+                            ->preserveFilenames()
+                            ->downloadable()
+                            ->image(),
+                    ]),
+
+                section::make('Detail tampilan')
+                    ->columns(2)
+                    ->schema([
+                        Toggle::make('is_active')
+                            ->required(),
+                        TextInput::make('sort_order')
+                            ->required()
+                            ->numeric()
+                            ->default(0),
+                    ]),
+
+                section::make('Konfigurasi SEO')
+                    ->schema([
+                        TextInput::make('meta_title')
+                            ->default(null),
+                        Textarea::make('meta_description')
+                            ->default(null)
+                            ->columnSpanFull(),
+                    ]),
+            ]);
+    }
+}
